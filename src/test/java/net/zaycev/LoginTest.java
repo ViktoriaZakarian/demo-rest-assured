6857bac07de9d1ae6bcc2efa52d2a9d92accd8c2
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-10 22:36:42 +0300

    add LoginTest test1

---
+++ src/test/java/net/zaycev/LoginTest.java
@@ -0,0 +1,22 @@
+package net.zaycev;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class LoginTest {
+
+    @Test
+    public void test1() {
+        given()
+                .headers(LoginRequest.getHeaders())
+                .body(LoginRequest.getBody("qwert@gmail.com", "qwert"))
+                .when()
+                .post(LoginRequest.URL_LOGIN)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("error" , equalTo("wrong login or password"));
+    }
+}
