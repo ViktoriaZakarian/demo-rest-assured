cad9304d6f57f01157b051fbd3937f1da683161a
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-10 22:34:19 +0300

    add LoginRequest

---
+++ src/test/java/net/zaycev/LoginRequest.java
@@ -0,0 +1,19 @@
+package net.zaycev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest {
+
+    public static final String URL_LOGIN = "https://zaycev.net/api/external/auth/login";
+
+    public static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json;charset=UTF-8");
+        return headers;
+    }
+
+    public static String getBody(String email, String password) {
+        return "{\"emailOrLogin\":\"" + email + "\",\"password\":\"" + password + "\"}";
+    }
+}
