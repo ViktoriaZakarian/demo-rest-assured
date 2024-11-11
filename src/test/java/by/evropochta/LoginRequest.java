bef76759d1c7b253cbc9417e8d381839aaa57bd4
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-11 23:07:07 +0300

    refactor LoginRequest (evropochta) add method getBody

--- src/test/java/by/evropochta/LoginRequest.java
+++ src/test/java/by/evropochta/LoginRequest.java
@@ -7,4 +7,8 @@ public class LoginRequest {
     public static String getBody(String login, String password) {
         return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\",\"Data\":{\"LoginName\":\"" + login + "\",\"Password\":\"" + password + "\",\"LoginNameTypeId\":\"2\"}}}";
     }
+
+    public static String getBody() {
+        return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\",\"Data\":{\"LoginName\":\"\",\"Password\":\"\",\"LoginNameTypeId\":\"2\"}}}";
+    }
 }
