b3bdcfc4c679b367e86173173ed6af4243f46045
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-11 23:31:42 +0300

    refactor LoginRequest (evropochta)

--- src/test/java/by/evropochta/LoginRequest.java
+++ src/test/java/by/evropochta/LoginRequest.java
@@ -11,4 +11,24 @@ public class LoginRequest {
     public static String getBody() {
         return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\",\"Data\":{\"LoginName\":\"\",\"Password\":\"\",\"LoginNameTypeId\":\"2\"}}}";
     }
+
+    public static String getBodyNoFieldLogin() {
+        return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\",\"Data\":{\"Password\":\"\",\"LoginNameTypeId\":\"2\"}}}";
+    }
+
+    public static String getBodyNoFieldPassword() {
+        return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\",\"Data\":{\"LoginName\":\"\",\"LoginNameTypeId\":\"2\"}}}";
+    }
+
+    public static String getBodyNoData() {
+        return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\"}}";
+    }
+
+    public static String getBodyNoNumberService() {
+        return "{\"CRC\":\"\",\"Packet\":{\"MethodName\":\"GetJWT\",\"JWT\":null,\"Data\":{\"LoginName\":\"\",\"Password\":\"\",\"LoginNameTypeId\":\"2\"}}}";
+    }
+
+    public static String getBodyNoPacket() {
+        return "{\"CRC\":\"\",\"ServiceNumber\":\"E811AE79-DFDE-4F85-8715-DD3A8308707E\",\"Data\":{\"LoginName\":\"\",\"Password\":\"\",\"LoginNameTypeId\":\"2\"}}";
+    }
 }
