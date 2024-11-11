0431f4284f104a6aca01630ec34f86f4a568fbe5
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-11 23:08:06 +0300

    refactor LoginTest (evropochta) add testNoPassword, testNoLogin, testNoLoginNoPassword

--- src/test/java/by/evropochta/LoginTest.java
+++ src/test/java/by/evropochta/LoginTest.java
@@ -16,9 +16,51 @@ public class LoginTest {
     }
 
     @Test
-    public void test1() {
+    public void testLoginAndPassword() {
         given()
                 .body(LoginRequest.getBody("375251234567", "qazwsx"))
+                .when()
+                .post(LoginRequest.URL_LOGIN)
+                .then()
+                .contentType("text/plain;charset=UTF-8")
+                .log().all()
+                .statusCode(200)
+                .body("Table[0].Error", equalTo (50009))
+                .body("Table[0].ErrorDescription", equalTo("Неверный логин или пароль"));
+    }
+
+    @Test
+    public void testNoPassword() {
+        given()
+                .body(LoginRequest.getBody("375298888888", ""))
+                .when()
+                .post(LoginRequest.URL_LOGIN)
+                .then()
+                .contentType("text/plain;charset=UTF-8")
+                .log().all()
+                .statusCode(200)
+                .body("Table[0].Error", equalTo (50009))
+                .body("Table[0].ErrorDescription", equalTo("Неверный логин или пароль"));
+    }
+
+    @Test
+    public void testNoLogin() {
+        given()
+                .body(LoginRequest.getBody("", "zxcvbb"))
+                .when()
+                .post(LoginRequest.URL_LOGIN)
+                .then()
+                .contentType("text/plain;charset=UTF-8")
+                .log().all()
+                .statusCode(200)
+                .body("Table[0].Error", equalTo (50009))
+                .body("Table[0].ErrorDescription", equalTo("Неверный логин или пароль"));
+    }
+
+    @Test
+    public void testNoLoginNoPassword() {
+        given()
+                .body(LoginRequest.getBody())
                 .when()
                 .post(LoginRequest.URL_LOGIN)
                 .then()
